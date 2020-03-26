983308c991fb3a74cf99cb9fc7c44601c59143b9
Kyle <kylepcole@gmail.com>
2020-03-26 17:46:47 -0400

    changed icon package

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <KitchenContainer id="1"/>
-  	  <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+  	  <div>Icons by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a></div>
     </div>
   );
 }
